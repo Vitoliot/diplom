5d8a6a1e79049c178ec90b25830476908f210436
Vitoliot <vpanaet@outlook.com>
2022-01-16 20:21:13 +0300

    add Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import {Route, Switch} from 'react-router-dom';
 import TaskCatalog from "./components/TaskCatalog/TaskCatalog"
+import Footer from "./components/Footer/Footer";
 
 function App () {
+    console.log(Footer)
     return <section className="App">
         {/* <Switch> */}
         {/* <Route path="/taskCatalog" component={TaskCatalog} /> */}
     {/* </Switch> */}
     <TaskCatalog></TaskCatalog>
+    <Footer></Footer>
     </section>
 }
 
--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,4 +1,21 @@
+import React from "react";
+import styles from "./Footer.css";
+import CSSModules from "react-css-modules";
+import {Link} from 'react-router-dom';
+
+const Footer = () => {
+    return <footer className="footer" styleName="footer">
+        <div className="logo" styleName="logo"> 
+            <img src="../../../static/images/logo.svg"></img>
+        </div>
+        <div className="links" styleName="links">
+            <Link to={'/taskCatalog'} styleName='link'> {'К заданиям'}</Link>
+            <Link to={'/courseCatalog'} styleName='link'> {'К курсам'}</Link>
+        </div>
+    </footer>
+}
 
 
+export default CSSModules(Footer, styles, {allowMultiple : true})
 // TODO
 // Те же ссылки, что и в хедере + Лого; Справа мб ссылки на соц. сети автора
